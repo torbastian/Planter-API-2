d5f528a4bf1eff4cc5233c3b243a35fc5b26df18
Tor-bastian Hansen <torbastianhansen@hotmail.com>
2020-09-09 09:30:13 +0200

    Ny Migration

--- Planter API 2/Migrations/20200909072844_notNull.Designer.cs
+++ Planter API 2/Migrations/20200909072844_notNull.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Planter_API_2.Models;
 
 namespace Planter_API_2.Migrations
 {
     [DbContext(typeof(PlantsContext))]
-    partial class PlantsContextModelSnapshot : ModelSnapshot
+    [Migration("20200909072844_notNull")]
+    partial class notNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
